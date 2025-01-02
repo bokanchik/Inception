33802203f8d9bcdc9148f473fc1765753865769f
Alex <bokanchaalexandra@gmail.com>
2025-01-02 20:23:45 +0100

    mariadb kinda working

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
-# Start MariaDB in the background
-mkdir -p /var/run/mysqld && chown mysql:mysql /var/run/mysqld
-
-/etc/init.d/mariadb start 
+/etc/init.d/mariadb start
 
 sleep 5
-mysql -u root <<EOF
 
-CREATE DATABASE \`${DB_DATABASE}\`;
-CREATE USER \`${DB_ADMIN_USER}\`@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';
-GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_ADMIN_USER}\`@'%';
-FLUSH PRIVILEGES;
+mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE USER IF NOT EXISTS \`${DB_ADMIN_USER}\`@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';"
+mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_ADMIN_USER}\`@'%';"
+mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"FLUSH PRIVILEGES;"
 
+# close the service
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
 
-SHOW DATABASES;
-SELECT User, Host FROM mysql.user;
-
-EOF
-
-exit
+# restart the service 
+exec mysqld_safe
