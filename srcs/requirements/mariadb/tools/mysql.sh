77daf939d4a168ddd9b2e76190f220e194fe76dc
albokanc <bokanchaalexandra@gmail.com>
2025-01-06 17:56:05 +0100

    conf php-fpm + fix nginx conf

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -4,10 +4,10 @@
 
 sleep 5
 
-mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
-mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE USER IF NOT EXISTS \`${DB_ADMIN_USER}\`@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';"
-mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_ADMIN_USER}\`@'%';"
-mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"FLUSH PRIVILEGES;"
+mysql -u root -p${DB_ADMIN_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
+mysql -u root -p${DB_ADMIN_PASSWORD} -e"CREATE USER IF NOT EXISTS \`${DB_ADMIN_USER}\`@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';"
+mysql -u root -p${DB_ADMIN_PASSWORD} -e"GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_ADMIN_USER}\`@'%';"
+mysql -u root -p${DB_ADMIN_PASSWORD} -e"FLUSH PRIVILEGES;"
 
 # close the service
 mysqladmin -u root -p${DB_ADMIN_PASSWORD} shutdown
