ad6e1ff94e280b1bc1c87fcfb50566c9472cf364
albokanc <bokanchaalexandra@gmail.com>
2025-01-07 17:51:30 +0100

    wp-php.congif problem ?

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+sleep 10
+
+wp config create --allow-root \
+	--dbname=$DB_DATABASE \
+	--dbuser=$DB_ADMIN_USER \
+	--dbpass=$DB_ADMIN_PASSWORD \
+	--dbhost=mariadb:3306 --path='/var/www/wordpress'
