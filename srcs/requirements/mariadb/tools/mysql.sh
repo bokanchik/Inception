4478bb1c4b7df4738939e0417d2ba9eb45b2e9cd
Alexandra Bokancha <albokanc@bess-f4r3s3.clusters.42paris.fr>
2025-01-14 14:09:16 +0100

    changes to conf

--- srcs/requirements/mariadb/tools/mysql.sh
+++ srcs/requirements/mariadb/tools/mysql.sh
@@ -7,10 +7,11 @@ sleep 5
 mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE DATABASE IF NOT EXISTS \`${DB_DATABASE}\`;"
 mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"CREATE USER IF NOT EXISTS \`${DB_ADMIN_USER}\`@'%' IDENTIFIED BY '${DB_ADMIN_PASSWORD}';"
 mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"GRANT ALL PRIVILEGES ON \`${DB_DATABASE}\`.* TO \`${DB_ADMIN_USER}\`@'%';"
+mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';"
 mysql -h localhost -u root -p${DB_ADMIN_PASSWORD} -e"FLUSH PRIVILEGES;"
 
 # close the service
 mysqladmin -u root -p${DB_ADMIN_PASSWORD} shutdown
 
-# restart the service 
+# to keep the container running
 exec mysqld_safe
